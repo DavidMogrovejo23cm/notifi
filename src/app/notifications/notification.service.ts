80d1d28dd3e94f9d65f1cf8f195c98bb72c03eb1
DavidMogrovejo23cm <ldmogrovejo@sudamericano.edu.ec>
2025-06-05 22:36:41 -0500

    segundo

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiUrl = 'http://localhost:3000/notifications';
+  private apiUrl = 'https://trabajopoo-production.up.railway.app/';
 
   constructor(private http: HttpClient) {}
 
